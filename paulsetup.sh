f684399adeff4042a17dfdb8fc6493557e5b0be9
palkiakerr <mxwkerr@gmail.com>
2021-06-14 19:58:00 +0100

    Update paulsetup.sh

--- paulsetup.sh
+++ paulsetup.sh
@@ -1,28 +1,27 @@
 # Install Rust alts
-pacman -S exa bat --noconfirm
+pacman -S exa bat
 
 # Install Starship Prompt and .bashrc
-pacman -S ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono --noconfirm
+pacman -S ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 mv bashrc ~/.bashrc
 
 # Install Emacs
-sudo pacman -S ttf-fira-code emacs --noconfirm
+sudo pacman -S ttf-fira-code emacs
 mkdir ../.emacs.d
 mv init.el ../.emacs.d/init.el
 
 # Install Terminal
-sudo pacman -S alacritty --noconfirm
+sudo pacman -S alacritty
 echo "background_opacity: 0.95" >> ~/.config/alacritty.yml
 
 # Configure i3 and it's bar
-mv i3config ~/.i3/config
-mv i3status ~/.config/i3status/config
+mv i3config ../.i3/config
+mv i3status ../.config/i3status/config
 
 # Install Other Useful Software (some is necessary for the previous configs)
-pacman -S deepin-screenshot mpv youtube-dl rofi transmission-gtk croc code blender --noconfirm
+#yay -S brave spotify zoom fluent-reader
+pacman -S deepin-screenshot mpv youtube-dl rofi transmission-gtk croc code blender
 
 cd
 git clone https://github.com/palkiakerr/wallpapers
-
-echo "Please Run The Following: yay -S brave spotify zoom fluent-reader"
